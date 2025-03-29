da8c242f30cadedf3f93a47a62e64492c565ba46
Tanamit <tanamit01@gmail.com>
2025-03-29 14:33:06 +0700

    Initial commit with HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld { //คลาสหลักของโปรแกรม
+    public static void main(String[] args) { // Method เริ่มต้น
+        System.out.println("Welcome to Java"); // คำสั่งแสดงผลข้อความ
+        System.out.println("Let's have fun!");
+        System.out.println("From: Tanamit");
+    }
+}
