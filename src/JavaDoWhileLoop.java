71195000b62f39e043a33afe5b28ce652306152c
Tanamit <tanamit01@gmail.com>
2025-04-03 13:36:34 +0700

    add JavaDoWhileLoop

---
+++ src/JavaDoWhileLoop.java
@@ -0,0 +1,9 @@
+public class JavaDoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Round " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
