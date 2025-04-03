962cb7d06f5237759b191f53c8975c1c98b652a5
Tanamit <tanamit01@gmail.com>
2025-04-03 13:34:20 +0700

    add JavaWhileLoop

---
+++ src/JavaWhileLoop.java
@@ -0,0 +1,9 @@
+public class JavaWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Round " + i);
+            i++;
+        }
+    }
+}
